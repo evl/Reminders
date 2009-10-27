257b84b394c34bd140bd358f8502941e5c18cfba
Evl <wow.evl@gmail.com>
2009-10-27 13:02:32 +0100

    Completely reworked how reminders update by switching from a timer based system to events, this should significantly improve performance.

--- evl_Reminders.lua
+++ evl_Reminders.lua
@@ -13,54 +13,17 @@ evl_Reminders.config = {
 	}
 }
 
-local UPDATE_INTERVAL = 0.5
-local ICON_PATH = "Interface\\Icons\\"
-local DEFAULT_ICON = "Temp"
-
 local config = evl_Reminders.config
-local lastUpdate = 0
 local reminders = {}
-local menuFrame = CreateFrame("Frame", "evl_RemindersMenu", UIParent, "UIDropDownMenuTemplate")
-local menu = {}
 
-local supressReminder = function(self, reminder)
-	reminder.active = false
-	reminder.reactivateTime = GetTime() + 300
-end
-
-local disableReminder = function(self, reminder)
-	reminder.active = false
-end
-
-local onEvent = function(self)
-	self:SetScale(config.scale)
-	self:SetPoint(config.position[1], config.position[2], config.position[3], config.position[4], config.position[5])
-end
-
-local onUpdate = function(self, elapsed)
-	if InCombatLockdown() then return end
-
-	lastUpdate = lastUpdate + elapsed
-
-	if lastUpdate > UPDATE_INTERVAL then
-		lastUpdate = 0
-
-		for _, reminder in ipairs(reminders) do
-			if not reminder.active and (reminder.reactivateTime > 0 and GetTime() >= reminder.reactivateTime) then
-				reminder.active = true
-				reminder.reactivateTime = 0
-			end
-
-			local result = reminder.active and reminder.callback()
-
-			if result then
-				reminder:Show()
-			else
-				reminder:Hide()
-			end
-		end
-
-		self:UpdatePositions()
+local handler
+local onEvent = function(self, event, ...)
+	handler = self[event]
+	
+	if handler then
+		handler(self, event, ...)
+	else
+		self:UpdateReminders()
 	end
 end
 
@@ -111,16 +74,24 @@ local onLeave = function(self)
 	GameTooltip:Hide()
 end
 
+local disableReminder = function(self, reminder, reactivateTime)
+	reminder.active = false
+	reminder.reactivateTime = reactivateTime and (GetTime() + reactivateTime) or 0
+
+	evl_Reminders:UpdateReminders()
+end
+
+local menu
+local menuFrame = CreateFrame("Frame", "evl_RemindersMenu", UIParent, "UIDropDownMenuTemplate")
 local onClick = function(self)
 	menu = {
 		{text = self.name, isTitle = true},
-		{text = "Supress for 5 minutes", func = supressReminder, arg1 = self},
+		{text = "Suppress for 5 minutes", func = disableReminder, arg1 = self, arg2 = 300},
 		{text = "Disable for this session", func = disableReminder, arg1 = self}
 	}
 
 	EasyMenu(menu, menuFrame, "cursor", nil, nil, "MENU")
 end
-
 
 -- Utility functions
 function evl_Reminders:IsInPartyWith(class)
@@ -159,7 +130,7 @@ function evl_Reminders:PlayerHasBuff(name)
 	return false
 end
 
-function evl_Reminders:AddReminder(name, callback, icon, attributes, tooltip, color)
+function evl_Reminders:AddReminder(name, event, callback, icon, attributes, tooltip, color)
 	local buttonName = "ReminderButton" .. #reminders
 	local frame = CreateFrame("Button", buttonName, self, "SecureActionButtonTemplate, ActionButtonTemplate")
 
@@ -172,7 +143,7 @@ function evl_Reminders:AddReminder(name, callback, icon, attributes, tooltip, co
 
 	local texture = frame:CreateTexture(nil, "BACKGROUND")
 	texture:SetAllPoints(frame)
-	texture:SetTexture(ICON_PATH .. (icon or DEFAULT_ICON))
+	texture:SetTexture("Interface\\Icons\\" .. (icon or "Temp"))
 	texture:SetTexCoord(.07, .93, .07, .93)
 	
 	if color then
@@ -193,33 +164,66 @@ function evl_Reminders:AddReminder(name, callback, icon, attributes, tooltip, co
 
 	frame:SetAttribute("alt-type*", "showmenu")
 	frame.showmenu = onClick
-
+	
 	table.insert(reminders, frame)
+
+	self:RegisterEvent(event)
+
 	return frame
 end
 
-function evl_Reminders:UpdatePositions()
-	local previousReminder = nil
-
-	for _, reminder in ipairs(reminders) do
-		if reminder:IsVisible() then
-			reminder:ClearAllPoints()
-
-			if previousReminder then
-				reminder:SetPoint("LEFT", previousReminder, "RIGHT", 5, 0)
-			else
-				reminder:SetPoint("TOPLEFT", self)
+function evl_Reminders:UpdateReminders(event, unit)
+	if not InCombatLockdown() then
+		local result
+		local previousReminder
+		
+		for _, reminder in ipairs(reminders) do
+			if not reminder.active and (reminder.reactivateTime > 0 and GetTime() >= reminder.reactivateTime) then
+				reminder.active = true
+				reminder.reactivateTime = 0
 			end
 
-			previousReminder = reminder
+			result = reminder.active and reminder.callback()
+
+			if result then
+				reminder:ClearAllPoints()
+
+				if previousReminder then
+					reminder:SetPoint("LEFT", previousReminder, "RIGHT", 5, 0)
+				else
+					reminder:SetPoint("TOPLEFT", self)
+				end
+
+				reminder:Show()
+
+				previousReminder = reminder
+			else
+				reminder:Hide()
+			end
 		end
 	end
+end
+
+function evl_Reminders:PLAYER_ENTERING_WORLD(event)
+	self:SetScale(config.scale)
+	self:SetPoint(config.position[1], config.position[2], config.position[3], config.position[4], config.position[5])
+
+	self:UpdateReminders()
+end
+
+function evl_Reminders:UNIT_AURA(event, unit)
+	if unit == "player" then
+		self:UpdateReminders()
+	end
+end
+
+function evl_Reminders:UNIT_INVENTORY_CHANGED(event, unit)
+	self:UNIT_AURA(event, unit)
 end
 
 evl_Reminders:SetWidth(36)
 evl_Reminders:SetHeight(36)
 
 evl_Reminders:SetScript("OnEvent", onEvent)
-evl_Reminders:SetScript("OnUpdate", onUpdate)
-
 evl_Reminders:RegisterEvent("PLAYER_ENTERING_WORLD")
+evl_Reminders:RegisterEvent("PLAYER_REGEN_ENABLED")
--- modules/mage.lua
+++ modules/mage.lua
@@ -1,4 +1,6 @@
 if select(2, UnitClass("player")) == "MAGE" then	
-	evl_Reminders:AddReminder("Less than 3 Mana Gems remaining", function() return GetItemCount("Mana Sapphire", false, true) < 3 end, "INV_Misc_Gem_Sapphire_02",  {type1 = "spell", spell1 = "Conjure Mana Gem"})
-	evl_Reminders:AddReminder("Armor missing", function() return not evl_Reminders:PlayerHasBuff("Molten Armor") and not evl_Reminders:PlayerHasBuff("Mage Armor") end, "Ability_Mage_MoltenArmor", {type = "spell", spell1 = "Molten Armor",spell2 = "Mage Armor", unit = "player"})
+	evl_Reminders:AddReminder("Armor missing", "UNIT_AURA", function() return not evl_Reminders:PlayerHasBuff("Molten Armor") and not evl_Reminders:PlayerHasBuff("Mage Armor") end, "Ability_Mage_MoltenArmor", {type = "spell", spell1 = "Molten Armor",spell2 = "Mage Armor", unit = "player"})
+
+	-- TODO: Needs to be fixed so it works with all levels of mana gems, please submit a patch!
+	--evl_Reminders:AddReminder("Less than 3 Mana Gems remaining", "UNIT_INVENTORY_CHANGED", function() return GetItemCount("Mana Sapphire", false, true) < 3 end, "INV_Misc_Gem_Sapphire_02",  {type1 = "spell", spell1 = "Conjure Mana Gem"})
 end
