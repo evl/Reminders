37f4a3fa9652873675ffcc8aac94ece6b46d9e67
Evl <wow.evl@gmail.com>
2009-12-14 10:14:36 +0100

    Removed some obsolete code.

--- modules/deathknight.lua
+++ modules/deathknight.lua
@@ -1,5 +1,3 @@
 if select(2, UnitClass("player")) == "DEATHKNIGHT" then
-	--if evl_Reminders:HasTalent()
-	--evl_Reminders:AddReminder("Missing Ghoul", function() )
-	--evl_Reminders:AddReminder("Missing Horn of Winter", function() return not evl_Reminders:PlayerHasBuff("Horn of Winter") end, "INV_Misc_Horn_02", {type1 = "spell", spell1 = "Horn of Winter", unit1 = "player"})
+	--evl_Reminders:AddReminder("Missing Ghoul", function())
 end
