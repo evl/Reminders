1c151b256f1425d928bc42f59588821bfbf4f747
Evl <wow.evl@gmail.com>
2010-09-11 17:38:57 +0200

    Updated Druid reminders.

--- modules/druid.lua
+++ modules/druid.lua
@@ -1,7 +1,7 @@
 local addonName, addon = ...
 local config = addon.config.druid
 
-if config.enabled and addon.playerClass == "DRUID" then	
-	addon:AddReminder("Missing Mark of the Wild", function() return not addon:PlayerHasAnyAura({"Mark of the Wild", "Gift of the Wild"}) end, "Spell_Nature_Regeneration", {type = "spell", unit = "player", spell1 = "Mark of the Wild", spell2 = "Gift of the Wild"})
-	addon:AddReminder("Missing Thorns", function() return not addon:PlayerHasBuff("Thorns") end, "Spell_Nature_Thorns", {type = "spell", spell = "Thorns", unit = "player"})
+if config.enabled and addon.playerClass == "DRUID" then
+	addon:AddReminder("Missing Mark of the Wild", "UNIT_AURA", function() return not addon:HasAnyAura({"Mark of the Wild", "Gift of the Wild"}) end, "spell_nature_regeneration", nil, {type = "spell", unit = "player", spell1 = "Mark of the Wild", spell2 = "Gift of the Wild"})
+	addon:AddReminder("Missing Thorns", "UNIT_AURA", function() return not UnitAura("player", "Thorns") end, "spell_nature_thorns", nil, {type = "spell", spell = "Thorns", unit = "player"})
 end
