1c84522df0158dd15ae004fedbe0d83d4eab0432
Evl <wow.evl@gmail.com>
2010-10-03 10:30:58 +0200

    Fixed a typo.

--- utilities.lua
+++ utilities.lua
@@ -63,7 +63,7 @@ function addon:HasEnchantableWeapon(slot)
 	local link = GetInventoryItemLink("player", slot)
 	local quality = select(3, GetItemInfo(link))
 	
-	if link quality > 1 then
+	if link and quality > 1 then
 		local subClass, _, equipSlot = select(7, GetItemInfo(link))
 		local localizedSlot = _G[equipSlot]
 		
