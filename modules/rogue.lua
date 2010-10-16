54db1c0adac731964daa3e8d2a3ca9fabaa28ac5
Evl <wow.evl@gmail.com>
2010-10-16 13:09:12 +0200

    Removed poison ranks.

--- modules/rogue.lua
+++ modules/rogue.lua
@@ -2,81 +2,10 @@ local addonName, addon = ...
 local config = addon.config.rogue
 
 if config.enabled and addon.playerClass == "ROGUE" then
-	-- Poisons
-	local poisons = {
-		["Anesthetic Poison"] = {
-			{level = 68, itemId = 21835},
-			{level = 77, itemId = 43237}
-		},
-
-		["Crippling Poison"] = {
-			{level = 20, itemId = 3775}
-		},
-
-		["Deadly Poison"] = {
-			{level = 30, itemId = 2892},
-			{level = 38, itemId = 2893},
-			{level = 46, itemId = 8984},
-			{level = 54, itemId = 8985},
-			{level = 60, itemId = 20844},
-			{level = 62, itemId = 22053},
-			{level = 70, itemId = 22054},
-			{level = 76, itemId = 43232},
-			{level = 80, itemId = 43233}
-		},
-
-		["Instant Poison"] = {
-			{level = 20, itemId = 6947},
-			{level = 28, itemId = 6949},
-			{level = 36, itemId = 6950},
-			{level = 44, itemId = 8926},
-			{level = 52, itemId = 8927},
-			{level = 60, itemId = 8928},
-			{level = 68, itemId = 21927},
-			{level = 73, itemId = 43230},
-			{level = 79, itemId = 43231}
-		},
-
-		["Mind-numbing Poison"] = {
-			{level = 24, itemId = 5237}
-		},
-
-		["Wound Poison"] = {
-			{level = 32, itemId = 10918},
-			{level = 40, itemId = 10920},
-			{level = 48, itemId = 10921},
-			{level = 56, itemId = 10922},
-			{level = 64, itemId = 22055},
-			{level = 72, itemId = 43234},
-			{level = 78, itemId = 43235}
-		}
-	}
-
-	local getPoisonItemString = function(poison)
-		local poisonData = poisons[poison]
-
-		if poisonData then
-			local level = UnitLevel("player")
-			local itemId
-
-			for _, rank in ipairs(poisonData) do
-				if level >= rank.level then
-					itemId = rank.itemId
-				else
-					break
-				end
-			end
-
-			if itemId then
-				return "item:" .. itemId
-			end
-		end
-	end
-		
 	local mainHandTooltip, offHandTooltip, throwingTooltip = addon:GetWeaponEnchantTooltip(config.mainHandPoisons[1], config.mainHandPoisons[2]), addon:GetWeaponEnchantTooltip(config.offHandPoisons[1], config.offHandPoisons[2]), addon:GetWeaponEnchantTooltip(config.throwingPoisons[1], config.throwingPoisons[2])
-	local mainHandAttributes = {type = "item", ["target-slot"] = 16, item1 = getPoisonItemString(config.mainHandPoisons[1]), item2 = getPoisonItemString(config.mainHandPoisons[2]), threshold = config.thresholdTime}
-	local offHandAttributes = {type = "item", ["target-slot"] = 17, item1 = getPoisonItemString(config.offHandPoisons[1]), item2 = getPoisonItemString(config.offHandPoisons[2]), threshold = config.thresholdTime}
-	local throwingAttributes = {type = "item", ["target-slot"] = 18, item1 = getPoisonItemString(config.throwingPoisons[1]), item2 = getPoisonItemString(config.throwingPoisons[2]), threshold = config.thresholdTime}
+	local mainHandAttributes = {type = "item", ["target-slot"] = 16, item1 = config.mainHandPoisons[1], item2 = config.mainHandPoisons[2], threshold = config.thresholdTime}
+	local offHandAttributes = {type = "item", ["target-slot"] = 17, item1 = config.offHandPoisons[1], item2 = config.offHandPoisons[2], threshold = config.thresholdTime}
+	local throwingAttributes = {type = "item", ["target-slot"] = 18, item1 = config.throwingPoisons[1], item2 = config.throwingPoisons[2], threshold = config.thresholdTime}
 
 	addon:AddReminder("Main hand poison", addon.WeaponEnchantCallback, mainHandAttributes, nil, nil, mainHandTooltip)
 	addon:AddReminder("Off hand poison", addon.WeaponEnchantCallback, offHandAttributes, nil, nil, offHandTooltip)
@@ -113,15 +42,15 @@ if config.enabled and addon.playerClass == "ROGUE" then
 	
 	local poisonItems = {}
 	
-	for _, slot in pairs({config.mainHandPoisons, config.offHandPoisons}) do
+	for _, slot in pairs({config.mainHandPoisons, config.offHandPoisons, config.throwingPoisons}) do
 		for _, name in pairs(slot) do
-			poisonItems[name] = getPoisonItemString(name)
+			poisonItems[name] = select(10, GetItemInfo(name))
 		end
 	end
 	
 	local checkPoisonStock = function(self)
-		for _, itemString in pairs(poisonItems) do
-			if (GetItemCount(itemString) < config.restockThreshold) then
+		for name in pairs(poisonItems) do
+			if (GetItemCount(name) < config.restockThreshold) then
 				return true
 			end
 		end
@@ -131,19 +60,18 @@ if config.enabled and addon.playerClass == "ROGUE" then
 		local merchantItems = addon:GetMerchantItems()
 		
 		if merchantItems then
-			for name, itemString in pairs(poisonItems) do
-				local item = GetItemInfo(itemString)
-				local merchantIndex = merchantItems[item]
+			for name in pairs(poisonItems) do
+				local merchantIndex = merchantItems[name]
 				
 				if merchantIndex then
-					local count = GetItemCount(itemString)
+					local count = GetItemCount(name)
 					local maxStack = GetMerchantItemMaxStack(merchantIndex)
 					
 					while count < config.restockAmount do
 						local buyCount = math.min(maxStack, config.restockAmount - count)
 						
 						if addon.debug then
-							print("Buying", item, "x", buyCount, "count:", count)
+							print("Buying", name, "x", buyCount, "count:", count)
 						end
 						
 						BuyMerchantItem(merchantIndex, buyCount)
@@ -165,17 +93,16 @@ if config.enabled and addon.playerClass == "ROGUE" then
 		
 		local validMerchant
 		
-		for name, itemString in pairs(poisonItems) do
-			local item, _, _, _, _, _, _, _, _, texture = GetItemInfo(itemString)
-			local count = GetItemCount(itemString)
+		for name, texture in pairs(poisonItems) do
+			local count = GetItemCount(name)
 			local r, g, b = addon:ConditionColorGradient(math.max(1, count / config.restockThreshold))
 			
-			GameTooltip:AddDoubleLine(item, count, 1, 1, 1, r, g, b)
+			GameTooltip:AddDoubleLine(name, count, 1, 1, 1, r, g, b)
 			GameTooltip:AddTexture(texture)
 			
 			-- Check if we are at a merchant selling our preferred poisons
 			if not validMerchant then
-				validMerchant = addon:MerchantHasItem(item)
+				validMerchant = addon:MerchantHasItem(name)
 			end
 		end
 		
