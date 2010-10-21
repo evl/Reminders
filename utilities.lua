49727833d0207d19178f08cf90e589a8a9a67b0e
Evl <wow.evl@gmail.com>
2010-10-21 19:09:23 +0200

    Fixed glyph detection.

--- utilities.lua
+++ utilities.lua
@@ -49,10 +49,8 @@ function addon:HasTalentRank(tabIndex, talentIndex, rankRequired)
 end
 
 function addon:HasGlyph(id)
-	for i = 1, 6 do
-		local _, _, glyphSpell = GetGlyphSocketInfo(i)
-		
-		if glyphSpell == id then
+	for i = 1, 9 do
+		if select(4, GetGlyphSocketInfo(i)) == id then
 			return true
 		end
 	end
