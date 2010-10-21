6b3a2654e0a2dd9955a7048c57311159d8a43e86
Evl <wow.evl@gmail.com>
2010-10-21 20:04:43 +0200

    Remove thorns.

--- modules/druid.lua
+++ modules/druid.lua
@@ -3,5 +3,4 @@ local config = addon.config.druid
 
 if config.enabled and addon.playerClass == "DRUID" then
 	addon:AddReminder("Missing Mark of the Wild", function() return not addon:HasAnyAura({"Mark of the Wild", "Gift of the Wild"}) end, {type = "spell", unit = "player", spell1 = "Mark of the Wild", spell2 = "Gift of the Wild"})
-	addon:AddReminder("Missing Thorns", function() return not UnitAura("player", "Thorns") end, {type = "spell", spell = "Thorns", unit = "player"})
 end
