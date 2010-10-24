2896119a744248513b1c9b0faa5f5f9427dc181a
Evl <wow.evl@gmail.com>
2010-10-24 13:51:17 +0200

    Replace Gift of the Wild with Blessing of Kings

--- modules/druid.lua
+++ modules/druid.lua
@@ -2,5 +2,5 @@ local addonName, addon = ...
 local config = addon.config.druid
 
 if config.enabled and addon.playerClass == "DRUID" then
-	addon:AddReminder("Missing Mark of the Wild", function() return not addon:HasAnyAura({"Mark of the Wild", "Gift of the Wild"}) end, {type = "spell", unit = "player", spell1 = "Mark of the Wild", spell2 = "Gift of the Wild"})
+	addon:AddReminder("Missing Mark of the Wild", function() return not addon:HasAnyAura({"Mark of the Wild", "Blessing of Kings"}) end, {type = "spell", unit = "player", spell = "Mark of the Wild"})
 end
