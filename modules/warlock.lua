ea0e6ca771257aa207f2b3f6d9aeef1969007fdb
Evl <wow.evl@gmail.com>
2010-10-25 23:41:11 +0200

    Remove weapon enchant reminder.

--- modules/warlock.lua
+++ modules/warlock.lua
@@ -8,7 +8,6 @@ if config.enabled and addon.playerClass == "WARLOCK" then
 	end
 	
 	addon:AddReminder("Missing armor", function() return not addon:HasAnyAura(config.armors) end, {type = "spell", spell1 = config.armors[1], spell2 = config.armors[2]})
-	addon:AddReminder("Weapon enchant", addon.WeaponEnchantCallback, {type = "item", ["target-slot"] = 16, item1 = "Grand Firestone", item2 = "Grand Spellstone", threshold = config.thresholdTime}, nil, nil, addon:GetWeaponEnchantTooltip("Grand Firestone", "Grand Spellstone"))
 	
 	-- Soul Link
 	if addon:HasTalentRank(2, 9) then
